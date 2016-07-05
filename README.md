0d20edbb7be8c4389900addc30c7a9f2ce576c26
DeathAxe <deathaxe@web.de>
2016-07-05 20:30:28 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,103 @@
+# [Skins][home]
+[![The MIT License](https://img.shields.io/badge/license-MIT-orange.svg?style=flat-square)](http://opensource.org/licenses/MIT)
+
+With **Skins** users can change the theme and color scheme of **Sublime Text** together with only one command. A skin is nothing else but a collection of settings appied to Sublime Text, if a skin is selected. Skins can be provided in theme packages such as _Boxy Theme_ or they can be created by users themeselfs by saving the current settings as a _User Skin_ of any name.
+
+![scr](screenshot.gif)
+
+## End Users
+### General Usage
+Open the `command pannel` with <kbd>Ctrl+Shift+P</kbd> and type `Select Skin`, `Save User Skin` or `Delete User Skin`.
+
+
+### Settings
+By default only `color_scheme`, `theme`, `font_face` and `font_size` are stored by `Save User Skin`. If you want more settings to be saved, you can change the `skin-template` in the `Skins.sublime-settings`.
+
+**Example**
+
+```javascript
+"skin-template":
+{
+	"Preferences": ["color_scheme","theme","font_face","font_size" /* ... */ ],
+	"SublimeLinter": {
+		/* structured settings are supported, too */
+		"user": ["error_color","gutter_theme","warning_color"]
+	}
+	/* add more packages here */
+}
+```
+
+## Theme Developers
+### General
+`Skins` parses all `<AnyName>.sublime-skins` files in all packages. They are expected to store a list of `skins` - a collection of settings for sublime text and other packages. More than one sublime-skins file can exist in a package. The name of the file does not matter, but the names of the skins inside must be unique per package. The quick panel will show these names. The `Package` providing it is displayed in the second row as a kind of description.
+
+### File Format
+
+```javascript
+{
+	// skin
+	"Monokai": {
+
+		// Packages/User/Preferences.sublime-settings
+		"Preferences": {
+			"color_scheme": "Packages/Color Scheme - Default/Monokai.tmTheme",
+			"theme": "Default.sublime-theme",
+			// ...
+		},
+
+		// Packages/User/SublimeLinter.sublime-settings
+		"SublimeLinter": {
+			"user": {
+				// ...
+			}
+		}
+	},
+
+	// skin
+	"Monokai 2": {
+		// ...
+	},
+
+	// ...
+}
+```
+
+Each child node of a skin represents a `<NodeName>.sublime-settings` file with all settings to write to. Therefore settings can be provided not only for `Sublime Text` but for any installed package such as `SublimeLinter`, too. A skin must at least contain the `Preferneces` node with `color_scheme` and `theme` settings to be valid but can include any other setting accepted by `Sublime Text`.
+
+Settings with `Null` value, are deleted in the sublime-settings files.
+
+### Commands
+`Skins` exports the following `commands` to directly interact with all available skins. They can be used to create key bindings or command shortcuts to the most frequent used skins.
+
+#### Set Skin
+
+```javascript
+"command": "set_skin",
+"args": { "package": "Skins", "name": "Monokai" }
+```
+
+#### Save User Skin
+
+The following example will directly save the current look and feel as `Preset 01` in the `Packages/User/Saved Skins.sublime-skins` file.
+
+```javascript
+"command": "save_user_skin",
+"args": { "name": "Preset 01" }
+```
+
+#### Delete User Skin
+
+The following example will directly delete `Preset 01` from the `Packages/User/Saved Skins.sublime-skins` file.
+
+```javascript
+"command": "delete_user_skin",
+"args": { "name": "Preset 01" }
+```
+
+## Inspired by
+- [Theme Menu Switcher][themeswitcher] by chmln
+- [QuickThemes][quickthemes] by chrislongo
+
+[home]:						<https://github.com/deathaxe/sublime-skins>
+[themeswitcher]:	<https://github.com/chmln/sublime-text-theme-switcher-menu>
+[quickthemes]:		<https://github.com/chrislongo/QuickThemes>
---
+++ skins.py
@@ -0,0 +1,242 @@
+import sublime
+import sublime_plugin
+import os
+import json
+
+def absolute_user_path():
+    """
+    Return absolute path of the Packages/User
+    """
+    return sublime.packages_path() + os.sep + "User" + os.sep
+
+def is_skin_valid(Skin_data):
+    """
+    For a skin to be valid at least "color_scheme" and "theme" must exist!
+
+    args:
+        Skin_data - JSON object with all settings to apply for the skin.
+    """
+    try:
+        for key in ["color_scheme", "theme"]:
+            if not any(sublime.find_resources(os.path.basename(Skin_data["Preferences"][key]))):
+                return False
+        return True
+    except:
+        return False
+
+def load_skins(skins_file):
+    """
+    Open a <skins_file>.sublime-skins and read all valid skins from it.
+    """
+    return { name : obj
+        for name, obj in sublime.decode_value(
+            sublime.load_resource(skins_file)).items()
+        if is_skin_valid(obj) }
+
+def load_pkg_skins(pkg_name = None):
+    """
+    Load all valid skins from all <name>.sublime-skins of the package <pkg_name>
+    If <pkg_name> is None, all skins from all packages are loaded.
+    """
+    items = {}
+    for skins_file in sublime.find_resources("*.sublime-skins"):
+        if not pkg_name or pkg_name in skins_file:
+            for key, value in sublime.decode_value(sublime.load_resource(skins_file)).items():
+                if is_skin_valid(value):
+                    items[key] = value
+    return items
+
+def load_user_skins():
+    """
+    Open the "Saved Skins.sublime-skins" and read all valid skins from it.
+    """
+    return load_skins("Packages/User/Saved Skins.sublime-skins")
+
+def save_user_skins(skins):
+    """
+    Save the skins to the "Saved Skins.sublime-skins".
+    """
+    with open(absolute_user_path() + "Saved Skins.sublime-skins", "w") as f:
+        f.write(sublime.encode_value(skins, True))
+
+
+class SetSkinCommand(sublime_plugin.WindowCommand):
+
+    def __init__(self, window):
+        self.window = window
+        self.changeset = {}
+        self.settings = sublime.load_settings("Preferences.sublime-settings")
+
+    def run(self, package = None, name = None):
+        """
+        API entry point for command "set_skin"
+
+        If "set_skin" is called with both args "package" and "name", the provided
+        information will be used to directly switch to the desired skin.
+
+        If at least one of the args is None, a quick panel with all available skins
+        is displayed.
+
+        args:
+            package   - name of the package providing the skin or (User)
+            name      - name of the skin in the <skins>.sublime-skins file
+        """
+
+        if package and name:
+            try:
+                self.apply_settings([name, package], load_pkg_skins(package)[name])
+            except:
+                sublime.status_message("Can't switch to invalid skin!")
+        else:
+            current_skin = self.settings.get("skin")
+            selected_index = -1
+            idx = 0
+            skin_names = []
+            Skin_data = []
+
+            for skins_file in sublime.find_resources("*.sublime-skins"):
+                pkg = skins_file.split("/")[1]
+                for name, data in sublime.decode_value(sublime.load_resource(skins_file)).items():
+                    if is_skin_valid(data):
+                        skin_names.append([name, pkg])
+                        Skin_data.append(data)
+                        if current_skin == pkg + "/" + name:
+                            selected_index = idx
+                        idx += 1
+
+            self.changeset = {}
+            self.window.show_quick_panel(
+                items = skin_names,
+                selected_index = selected_index,
+                on_select = lambda x: self.on_select(skin_names[x], Skin_data[x], x < 0),
+                on_highlight = lambda x: self.on_highlight(Skin_data[x]))
+
+    def on_select(self, skin_name, Skin_data, abort):
+        if abort:
+            for key, val in self.changeset.items():
+                if val:
+                    self.settings.set(key, val)
+                else:
+                    self.settings.erase(key)
+
+            sublime.save_settings("Preferences.sublime-settings")
+
+        else:
+            self.apply_settings(skin_name, Skin_data)
+
+    def on_highlight(self, Skin_data):
+        """
+        Preview the theme and color scheme as soon as a skin is highlighted.
+        """
+        for key, val in Skin_data["Preferences"].items():
+            # backup settings before changing the first time
+            if not key in self.changeset:
+                self.changeset[key] = self.settings.get(key)
+
+            if val:
+                self.settings.set(key, val)
+            else:
+                self.settings.erase(key)
+
+    def apply_settings(self, skin_name, Skin_data):
+        """
+        Apply all settings
+        """
+        self.settings.set("skin", skin_name[1] + "/" + skin_name[0])
+        for pkg_name, skns in Skin_data.items():
+            try:
+                pkgs = sublime.load_settings(pkg_name + ".sublime-settings")
+                for key, val in skns.items():
+                    if type(val) is dict:
+                        pkgs.set(key, pkgs.get(key).update(val))
+                    elif val:
+                        pkgs.set(key, val)
+                    else:
+                        pkgs.erase(key)
+                sublime.save_settings(pkg_name + ".sublime-settings")
+            except:
+                pass
+
+
+class DeleteUserSkinCommand(sublime_plugin.WindowCommand):
+
+    def run(self, name = None):
+        skins = load_user_skins()
+        if name:
+            self.delete(skins, name)
+        else:
+            self.window.show_quick_panel(
+                items = [ [ skin, "User" ] for skin in sorted(skins.keys()) ],
+                on_select = lambda x: self.delete(skins, sorted(skins.keys())[x], x < 0))
+
+    @staticmethod
+    def delete(skins, skin, abort = False):
+        if not abort and skin in skins.keys():
+            del skins[skin]
+            save_user_skins(skins)
+            sublime.status_message("Skin " + skin + " deleted!")
+        else:
+            sublime.status_message("Skin not deleted!")
+
+
+class SaveUserSkinCommand(sublime_plugin.WindowCommand):
+
+    def run(self, name = None):
+        if name:
+            template = sublime.load_settings("Skins.sublime-settings").get("skin-template")
+            skins = load_user_skins()
+            # BUG: sublime.load_setting(...) does not work reliably here, I don't know why.
+            #      cascaded settings are sometimes read as Null-Value, so use JSON library.
+            skin = {}
+            for pkg, css in template.items():
+                with open(absolute_user_path() + pkg + ".sublime-settings") as f:
+                    pkgs = self.transform(json.load(f), css)
+                    if pkgs:
+                       skin[pkg] = pkgs
+
+            skins[name] = skin
+            save_user_skins(skins)
+            sublime.status_message("Saved skin " + name)
+        else:
+            self.window.show_input_panel("Enter skins name:", "",
+                                         self.run, None, None)
+
+    def transform(self, json, css):
+        """
+        Filter JSON object by a Style-Sheet
+
+        This function transforms the <json> object by recursivly
+        parsing it and returning only the child objects whose keys
+        match the values in the cascaded stylesheed <css>.
+
+        args:
+            json    The data source to filter
+            css     The stylesheet used as filter
+
+                    Each <object> must exist in <json>.
+                    Each <key> and its value is read from <json> and
+                    added to the returned object.
+
+                    EXAMPLE:
+                    <object> : [<key>, <key>, ...],
+                    <object> : {
+                        <object> : [<key>, <key>, ...]
+                    }
+        """
+        if json and css:
+            if type(css) is dict:
+                node = {}
+                for key, ss in css.items():
+                    value = self.transform(json[key], ss)
+                    # do not add empty objects
+                    if value:
+                        node[key] = value
+                return node
+
+            if type(css) is list:
+                return { key : json[key] for key in css if key in json }
+
+            elif css in json:
+                return { css : json[css] }
+
+        return None
