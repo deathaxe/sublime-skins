fe907946b3ea858777cdb6d289816c2703b8106b
Ihor Oleksandrov <ihodev@gmail.com>
2017-08-25 15:45:34 +0300

    Review skin validation
    Modifies skin validation in a way check for at least a valid theme or
    color scheme to be available instead of both together.
    
    Closes #8

--- skins.py
+++ skins.py
@@ -44,22 +44,27 @@ def validate_skin(skin_data):
         skin_data - JSON object with all settings to apply for the skin.
     """
     # check theme file
-    theme_name = os.path.basename(skin_data[PREF]["theme"])
+    theme_name = os.path.basename(skin_data[PREF].get("theme"))
     theme_ok = any(sublime.find_resources(theme_name))
     # check color scheme
-    path, tail = os.path.split(skin_data[PREF]["color_scheme"])
-    name = tail.replace(" (SL)", "")
-    color_schemes = sublime.find_resources(name)
-    if not color_schemes:
-        return False
-    # Try to find the exact path from *.skins file
-    resource_path = "/".join((path, name))
-    for found in color_schemes:
-        if found == resource_path:
-            return theme_ok
-    # Use the first found color scheme which matches 'name'
-    skin_data[PREF]["color_scheme"] = color_schemes[0]
-    return theme_ok
+    color_schemes = []
+    color_scheme_ok = False
+    color_scheme_name = skin_data[PREF].get("color_scheme")
+    if color_scheme_name:
+        path, tail = os.path.split(color_scheme_name)
+        name = tail.replace(" (SL)", "")
+        color_schemes = sublime.find_resources(name)
+    if color_schemes:
+        # Try to find the exact path from *.skins file
+        resource_path = "/".join((path, name))
+        for found in color_schemes:
+            if found == resource_path:
+                color_scheme_ok = True
+        # Use the first found color scheme which matches 'name'
+        if not color_scheme_ok:
+            skin_data[PREF]["color_scheme"] = color_schemes[0]
+            color_scheme_ok = True
+    return theme_ok or color_scheme_ok
 
 
 def load_user_skins():
